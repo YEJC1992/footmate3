09db8e6107a74fad6cdf0fab06db9f9acf13b3e8
YEJC1992 <40877068+YEJC1992@users.noreply.github.com>
2021-01-07 23:51:12 +0800

    Update settings.py

--- foodmate_scrapy/settings.py
+++ foodmate_scrapy/settings.py
@@ -15,9 +15,9 @@ catidname = '1799'
 hege = ''
 #开始时间 一个周一个周的范围搜索
 #'2015-01-12'
-timebegin = '2017-01-01'
+timebegin = '2017-08-01'
 
-scrapy_end_time = '2017-08-01'
+scrapy_end_time = '2018-03-01'
 
 page = 1
 
